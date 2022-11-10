d20f33e16d9a0f6444eaf19c5b9a08c23f569f40
nitesh kandari <niteshkandari@niteshs-MacBook-Air.local>
2022-11-10 21:58:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react"
+import Todo from "./Todo";  //here i m importing a component from diff 
+//file which i am gonna render inside my app function
+// also a react component can only return one div 
+// i cannot do like this, those two divs has to be enclosed inside a parent div
+//  const ExApp = () => {
+//   return (
+//     <div>
+//     <div></div>
+//     <div></div>
+//     </div>
+//   );
+//  }
+
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  const handleIncrement = () => {
+    // in set state here we can update the value in two ways
+    //1 is this one with a call back where we get the snapshot 
+    //of the previous value and this is also the recommended way -> 
+    setNumber(previous => {
+      return previous = previous + 1;
+    })
+    // 2 other way to update the value in set state is 
+    //here we just take the variable and directly update it , one disadvantage here is that we dont have 
+    // any idea about the previous value
+    setNumber(number++);
+  }
+  const handleDecrement = () => {
+    setNumber(previous => {
+      return previous = previous - 1;
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {number}
+
+      <button onClick={handleIncrement}>+</button>
+      <button onClick={handleDecrement}>-</button>
+
+      {/**** for rending a component inside other component we just have to write the name of 
+       * component , also when ever we create a component the first letter must be capital react would
+       * just not recognise it as a component
+       */}
+
+       <Todo/>
     </div>
   );
 }
---
+++ src/Todo.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+
+const Todo = () => {
+  const [toDoList, setTodoList] = useState([]);
+  const [value, setValue] = useState("");
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+  const handleAddToDoList = () => {
+    const oldList = [...toDoList];
+    oldList.push(
+      Object.assign({}, { value, id: Math.floor(Math.random() * 100000) })
+    );
+    setTodoList([...oldList]);
+  };
+
+  const handleDeleteElement = (id) => {
+    const oldList = [...toDoList];
+    setTodoList((previousState) => {
+      return (previousState = oldList.filter((data) => data.id != id));
+    });
+  };
+  return (
+    <div>
+      {/**in jsx its not necessary to close and input and img tag
+       * like this <input></input>
+       */}
+      <input value={value} onChange={(e) => handleChange(e)} />
+      <button onClick={handleAddToDoList}>add</button>
+      {/******jus like in angular we have ng for, for looping in an array and showing the data
+       * here we can just do inside a jsx and and use map method since its return type is and array
+       * we can see the changes in the ui , so when ever i click on add button it updates the toDoList array
+       * and since it is a useState the whole ui will re render again
+       */}
+      {toDoList.length > 0 &&
+        toDoList.map((data) => {
+          //also i can directly write styles in and element it just that i have to pass it like and object
+          return (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignContent: "center",
+              }}
+            >
+              <div 
+               style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  flexDirection:"column"
+               }}
+               key={data.id}>
+                value : {data.value} <br/>
+                id : {data.id}
+              </div>
+              <span
+                style={{
+                  fontWeight: 900,
+                  fontSize: 20,
+                  color: "red",
+                  cursor:  'pointer'
+                }}
+                onClick={() => handleDeleteElement(data.id)}
+              >
+                X
+              </span>
+              {/*onClick of the span element i m calling the function  handleDelete inside a empty arrow
+              function beacuse i need to pass a value to the function 
+              if i do like this onClik={handleDelet(data.id)} it will immedidately execute the function
+               */}
+            </div>
+          ); //here also i cannot return multiple children it must be surrounded by one parent element
+        })}
+    </div>
+  );
+};
+
+export default Todo;
